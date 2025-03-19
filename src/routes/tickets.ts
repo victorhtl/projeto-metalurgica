26109d54294350800c37ca8eb644e6243b5bbcf9
Victor Luna <vht.luna@gmail.com>
2025-03-19 20:57:20 -0300

    feat: add get /tickets endpoint

--- src/routes/tickets.ts
+++ src/routes/tickets.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import createTicket from '../actions/tickets/create';
+import fetchTickets from '../actions/tickets/fetch';
 
 export default async function userRouter(server: FastifyInstance) {
   server.post('/', createTicket);
+  server.get('/', fetchTickets);
 }
